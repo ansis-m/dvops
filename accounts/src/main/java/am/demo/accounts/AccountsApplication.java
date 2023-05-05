9e29686d20abf921b7ef39d7e7c1218f625e764c
ansis maleckis <maleckisnansis@gmail.com>
2023-05-05 12:12:59 +0300

    added refresh config thru an actuator/refresh post

--- accounts/src/main/java/am/demo/accounts/AccountsApplication.java
+++ accounts/src/main/java/am/demo/accounts/AccountsApplication.java
@@ -3,6 +3,7 @@ package am.demo.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScans({ @ComponentScan("am.demo.accounts.controller") })
 @EnableJpaRepositories("am.demo.accounts.repository")
 @EntityScan("am.demo.accounts.model")
+@RefreshScope
 public class AccountsApplication {
 
     public static void main(String[] args) {
